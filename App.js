3a6d6a34cb9ecae73f2f3bb6dbe69fa004342ccf
Tanisha Verma <76125836+tanishaverma17@users.noreply.github.com>
2024-02-23 01:44:55 +0530

    ep1

---
+++ App.js
@@ -0,0 +1,5 @@
+const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},  React.createElement("div", {}, " h1 tag")) )
+  
+const heading = React.createElement("h1", {id: "heading"}, "hello world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);    
