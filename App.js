bf9be64dc4d993a8b8634268a66dcaad8e5436e9
Tanisha Verma <76125836+tanishaverma17@users.noreply.github.com>
2024-02-26 21:42:21 +0530

    basics done

--- App.js
+++ App.js
@@ -1,5 +1,29 @@
-const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"},  React.createElement("div", {}, " h1 tag")) )
-  
-const heading = React.createElement("h1", {id: "heading"}, "hello world");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = (
+  <h1 className="head" id="heading">
+    HI, I am tanisha
+  </h1>
+);
+
+const number = 1000;
+
+
+const TitleComponent = ()=>(
+  <div>
+    {heading}
+    <h1>Tanisha Verma</h1>
+    <h3>hii</h3> 
+  </div>
+)
+
+const HeadingComponent = ()=>(
+   <>
+   <h1>React Functional component</h1>
+   <TitleComponent/> 
+   </>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);    
+root.render(<HeadingComponent/>); 
