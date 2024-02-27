7786c2e4bf1e3bc34efbc7557b7d899278e90265
Tanisha Verma <76125836+tanishaverma17@users.noreply.github.com>
2024-02-28 00:17:58 +0530

    react hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,29 @@
-import Card from "./RestCard"
-import resList from "../utils/mockData"
+import { useState } from "react";
+import Card from "./RestCard";
+import resList from "../utils/mockData";
 
-const Body = ()=>{
-    return(
-      <div>
-        <div className="search">Search</div>
-        <div className="rest-container">
-          {
-            resList.map(resData => <Card key={resData.info.id} resData={resData}/>)
-          }
-        </div>
+const Body = () => {
+  const [ListOfRest, setListOfRest] = useState(resList);
+
+  return (
+    <div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setListOfRest(ListOfRest.filter((res) => res.info.avgRating > 4));
+          }}
+        >
+          top Rated Resturants
+        </button>
       </div>
-    )
-} 
+      <div className="rest-container">
+        {ListOfRest.map((resData) => (
+          <Card key={resData.info.id} resData={resData} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
